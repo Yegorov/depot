3a79a9e639218f6ea3340ab4dae8eea6296c4965
Yegorov <yegorov0725@yandex.ru>
2017-11-28 17:39:16 +0300

    Add before action for check auth user, add test helper methods

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,17 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def login_as(user)
+    post login_url, params: { name: user.name, password: 'secret' }
+  end
+
+  def logout
+    delete logout_url
+  end
+
+  def setup
+    if respond_to?(:session)
+      login_as users(:one)
+    end
+  end
 end
